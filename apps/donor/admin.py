fb0f8da280d073f20c2e02f1ade78aecc90ac3cd
Jubel Ahmed <jubel35-1648@diu.edu.bd>
2020-06-06 19:41:12 +0600

    logo ,language & desing update

--- apps/donor/admin.py
+++ apps/donor/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Donor
+
+# Header and title change
+admin.site.site_header = 'Red Love Blood Society' 
+admin.site.site_title = 'Red Love Blood Society' 
+admin.site.index_title = 'Red Love Blood Society'
+
 # Register your models here.
+ 
+@admin.register(Donor)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name','phone','blood_group','status','birth_date')
+    list_display_links = ('name', 'blood_group')
+    search_fields = ('name','phone','blood_group','status')
+   
--- apps/donor/models.py
+++ apps/donor/models.py
@@ -1,5 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+BLOOD_GROUP = (
+    ('o+',"O+"),
+    ('o-',"O-"),
+    ('a+',"A+"),
+    ('a-',"A-"),
+    ('b+',"B+"),
+    ('b-',"B-"),
+    ('ab+',"AB+"),
+    ('ab-',"AB-"),  
+    
+)
+class Donor(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15,unique=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    blood_group = models.CharField(max_length=4,choices=BLOOD_GROUP, default='o+')
+    birth_date = models.DateField(null=True, blank=True)
+    present_address = models.CharField(max_length=255)
+    permanent_address = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    blood_donor_status = models.BooleanField(default=True)
+    objects = models.QuerySet()
+    
+    class Meta:
+        ordering=['name','blood_group']
+    
+    def get_absolute_url(self):
+        return reverse('donor-detail',args=[str(self.id)])
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+
 
 

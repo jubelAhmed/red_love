d3c54b8f4813786d77babd8852bf22d2c5eaa8c4
JubelAhmed <jubel35-1648@diu.edu.bd>
2024-06-02 02:00:38 +0100

    update env config and static files issues fixed

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'donation.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'donation.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
